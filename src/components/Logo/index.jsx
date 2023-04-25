4dc1aca26fb894427cbfac3200eba5c38dca0944
Bruno Riwerson Silva <riwerson@gmail.com>
2023-04-25 08:57:22 -0300

    feat: create temp logo

---
+++ src/components/Logo/index.jsx
@@ -0,0 +1,15 @@
+import './style.css';
+
+function Logo() {
+  return (
+    <a href="#top" className="logo-container">
+      <p className="logo">
+        <span className="logo-sup">{'</>'}</span>
+        <span className="logo-mid">BRS</span>
+        <span className="logo-sub">DevWeb</span>
+      </p>
+    </a>
+  );
+}
+
+export default Logo;
