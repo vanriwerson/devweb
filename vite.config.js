077840bd42cf719d077df7e68e3694d767bbf155
Bruno Riwerson Silva <riwerson@gmail.com>
2023-04-25 14:17:29 -0300

    try: deploy with gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [reactRefresh()],
   base: '/devweb/',
-  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
 })
