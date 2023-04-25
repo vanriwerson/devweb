0f9b23fa539dd76af69b9086d9433bcc6de9fe89
Bruno Riwerson Silva <riwerson@gmail.com>
2023-04-25 10:26:46 -0300

    fix: base repo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://vanriwerson.github.io/devweb',
+  base: '/devweb/',
   plugins: [react()],
 })
