1107a3c31d87e97f3320a04fb9f3773c513b00d6
Bruno Riwerson Silva <riwerson@gmail.com>
2023-04-25 09:00:49 -0300

    feat: adjust App orientation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import Header from './components/Header';
+import './App.css';
+
 function App() {
   return (
-    <>
+    <main className='app'>
+      <Header />
       <h1>Man at work</h1>
+      
       <p className="read-the-docs">
         New portfolio is coming
       </p>
-    </>
+    </main>
   )
 }
 
