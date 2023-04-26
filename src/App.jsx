2db63759b2ad434e4ebdb78ce59bd6937555947f
Bruno Riwerson Silva <riwerson@gmail.com>
2023-04-26 13:59:38 -0300

    feat: applying carousel on App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import Header from './components/Header';
 import './App.css';
 import Footer from './components/Footer';
+import SkillsCarousel from './components/SkillsCarousel';
 
 function App() {
   return (
     <main className='app'>
       <Header />
-      <h1>Man at work</h1>
-      
+
+      <h1>Boas-vindas ao meu Portifólio</h1>
+
+      <SkillsCarousel />
+
       <p className="read-the-docs">
         New portfolio is coming
       </p>
