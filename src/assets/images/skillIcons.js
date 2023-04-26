230c4cbc83ff130f6a26ff602bcf0b7a8558fe28
Bruno Riwerson Silva <riwerson@gmail.com>
2023-04-26 13:53:33 -0300

    feat: importing skill icons

---
+++ src/assets/images/skillIcons.js
@@ -0,0 +1,33 @@
+import html from '../images/skills/html.svg';
+import css from '../images/skills/css.svg';
+import javascript from '../images/skills/javascript.svg';
+import react from '../images/skills/react.svg';
+import vuejs from '../images/skills/vuejs.svg';
+import typescript from '../images/skills/typescript.svg';
+import git from '../images/skills/git.svg';
+import redux from '../images/skills/redux.svg';
+import testingLibrary from '../images/skills/testing-library.png';
+import docker from '../images/skills/docker.png';
+import mysql from '../images/skills/mysql.png';
+import java from '../images/skills/java.svg';
+import jest from '../images/skills/jest.png';
+import linux from '../images/skills/linux.svg';
+
+const skillIcons = [
+  { id: 1, src: html, alt: 'Html' },
+  { id: 2, src: css, alt: 'CSS' },
+  { id: 3, src: javascript, alt: 'Javascript' },
+  { id: 4, src: react, alt: 'ReactJS' },
+  { id: 5, src: vuejs, alt: 'VueJS' },
+  { id: 6, src: typescript, alt: 'TypeScript' },
+  { id: 7, src: git, alt: 'Git' },
+  { id: 8, src: redux, alt: 'Redux' },
+  { id: 9, src: testingLibrary, alt: 'Testing Libray' },
+  { id: 10, src: docker, alt: 'Docker' },
+  { id: 11, src: mysql, alt: 'MySQL' },
+  { id: 12, src: java, alt: 'Java' },
+  { id: 13, src: jest, alt: 'Jest' },
+  { id: 14, src: linux, alt: 'Linux' },
+];
+
+export default skillIcons;
