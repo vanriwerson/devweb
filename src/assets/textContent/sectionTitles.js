e3ec7a86d83a5487455df28320b997d75fac512b
Bruno Riwerson Silva <riwerson@gmail.com>
2023-05-02 15:03:41 -0300

    feat: section title variables

---
+++ src/assets/textContent/sectionTitles.js
@@ -0,0 +1,8 @@
+const sectionTitles = {
+  home: 'Boas-Vindas ao meu Portifólio',
+  about: 'Um pouco de minha história',
+  skills: 'Habilidades adquiridas',
+  projects: 'Alguns de meus projetos de orgulho',
+}
+
+export default sectionTitles;
