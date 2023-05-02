aa7e02d7049e9e752987fe02071afeaa4967973e
Bruno Riwerson Silva <riwerson@gmail.com>
2023-05-02 13:46:07 -0300

    fix: small style issues

--- src/components/SectionDivider/SectionDivider.jsx
+++ src/components/SectionDivider/SectionDivider.jsx
@@ -1,17 +1,17 @@
 import PropTypes from 'prop-types';
-import logoIcon from '../../assets/images/logo-dark.svg';
+import logoIcon from '../../assets/images/logo-light.svg';
 import './style.css';
 
 function SectionDivider({ anchorRef }) {
   return (
     <div className="divider-container" id={ anchorRef }>
-      <span className='divider-line'></span>
+      <span className='divider-line left'></span>
       <img
         className='logo-icon'
         src={ logoIcon }
         alt='BRS DevWeb logo'
       />
-      <span className='divider-line'></span>
+      <span className='divider-line right'></span>
     </div>
   );  
 }
