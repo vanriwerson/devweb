dbdad31ee1031a754e63f042ca2ca40e2f03609a
Bruno Riwerson Silva <riwerson@gmail.com>
2023-05-02 10:31:32 -0300

    feat: apllying navigation through sections

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,41 @@ import Header from './components/Header';
 import './App.css';
 import Footer from './components/Footer';
 import SkillsCarousel from './components/SkillsCarousel';
+import SectionDivider from './components/SectionDivider/SectionDivider';
 
 function App() {
   return (
     <main className='app'>
       <Header />
 
-      <h1>Boas-vindas ao meu Portifólio</h1>
+      <SectionDivider  anchorRef='home'/>
 
-      <SkillsCarousel />
+      <section className='page-section'>
+        <h1>Boas-vindas ao meu Portifólio</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem, dignissimos. Suscipit rem accusantium repellendus dicta a harum illum nobis aliquam.</p>
+      </section>
 
-      <p className="read-the-docs">
-        New portfolio is coming
-      </p>
+      <SectionDivider anchorRef='about'/>
+
+      <section className='page-section'>
+        <h1>Um pouco de minha história</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint eaque alias enim, sequi nostrum itaque numquam delectus fugiat aliquid voluptates sunt ipsa optio accusantium. Tempora nihil non rem ipsa pariatur aliquam molestias magnam minus voluptatem unde rerum consequuntur, obcaecati aspernatur excepturi accusamus saepe soluta, quis animi. Facere neque nesciunt odio.</p>
+      </section>
+
+      <SectionDivider anchorRef='skills'/>
+
+      <section className='page-section'>
+        <h1>Ferramentas que utilizo atualmente</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum vero facere quam aliquam id architecto commodi praesentium, sunt deleniti sit nesciunt minus saepe optio nemo dolores accusamus enim, laboriosam a?</p>
+        <SkillsCarousel />
+      </section>
+
+      <SectionDivider anchorRef='projects'/>
+
+      <section className='page-section'>
+        <h1>Alguns de meus projetos de orgulho</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident officia eaque soluta, voluptatum, eius recusandae culpa quibusdam obcaecati sapiente natus voluptates molestias blanditiis assumenda? Sint ea maxime nobis omnis odio nesciunt molestiae autem! Sequi quaerat numquam facere, architecto tempore deleniti recusandae! Fugit esse consequatur iure aut excepturi at? Molestiae, voluptates.</p>
+      </section>
 
       <Footer />
     </main>
