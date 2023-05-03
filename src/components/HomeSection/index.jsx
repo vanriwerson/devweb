81c01d1eeb196ce270cf6959bedc71b88221d7d1
Bruno Riwerson Silva <riwerson@gmail.com>
2023-05-03 10:20:34 -0300

    feat: fishing home section

--- src/components/HomeSection/index.jsx
+++ src/components/HomeSection/index.jsx
@@ -1,13 +1,26 @@
 import SectionTitle from '../SectionTitle';
+import ProfileImage from '../ProfileImage';
 import sectionTitles from '../../assets/textContent/sectionTitles';
 import './style.css';
+import CTAButton from '../CTAButton';
 
 function HomeSection() {
   return (
-    <section className='page-section'>
+    <section className='home'>
       <SectionTitle title={ sectionTitles.home } />
-      
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem, dignissimos. Suscipit rem accusantium repellendus dicta a harum illum nobis aliquam.</p>
+
+      <div className='business-card'>
+        <ProfileImage />
+
+        <div className="business-card-text">
+          <h2 className='title'>Muito prazer! Meu nome é Bruno Riwerson Silva</h2>
+          <p className='content'>Sou o desenvolvedor web full-stack que vai ouvir seu problema e gerar uma solução prática e eficaz para ele. Fico muito feliz por seu interesse em conhecer um pouco do meu trabalho.</p>
+
+          <CTAButton
+            labelText='Vamos transformar seu sonho em código?'
+          />
+        </div>
+      </div>
     </section>
   );  
 }
