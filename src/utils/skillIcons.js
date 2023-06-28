0497acc85950a3c2f4934259575f0a4b4a5c396c
Bruno Riwerson Silva <riwerson@gmail.com>
2023-06-28 15:54:55 -0300

    struct: moving arrays to utils

---
+++ src/utils/skillIcons.js
@@ -0,0 +1,41 @@
+import html from '../images/skills/html.svg';
+import css from '../images/skills/css.svg';
+import javascript from '../images/skills/javascript.svg';
+import react from '../images/skills/react.svg';
+import vuejs from '../images/skills/vuejs.svg';
+import typescript from '../images/skills/typescript.svg';
+import git from '../images/skills/git.svg';
+import redux from '../images/skills/redux.svg';
+import testingLibrary from '../images/skills/testing-library.png';
+import docker from '../images/skills/docker.svg';
+import mysql from '../images/skills/mysql.png';
+import java from '../images/skills/java.svg';
+import jest from '../images/skills/jest.png';
+import linux from '../images/skills/linux.svg';
+import mongodb from '../images/skills/mongodb.png';
+import golang from '../images/skills/golang.png';
+import nodejs from '../images/skills/nodejs.png';
+import python from '../images/skills/python.png';
+
+const skillIcons = [
+  { id: 1, src: git, alt: 'Git' },
+  { id: 2, src: html, alt: 'Html' },
+  { id: 3, src: css, alt: 'CSS' },
+  { id: 4, src: javascript, alt: 'Javascript' },
+  { id: 5, src: react, alt: 'ReactJS' },
+  { id: 6, src: vuejs, alt: 'VueJS' },
+  { id: 7, src: redux, alt: 'Redux' },
+  { id: 8, src: jest, alt: 'Jest' },
+  { id: 9, src: testingLibrary, alt: 'Testing Libray' },
+  { id: 10, src: typescript, alt: 'TypeScript' },
+  { id: 11, src: docker, alt: 'Docker' },
+  { id: 12, src: nodejs, alt: 'NodeJS' },
+  { id: 13, src: golang, alt: 'Golang' },
+  { id: 14, src: java, alt: 'Java' },
+  { id: 15, src: python, alt: 'Python' },
+  { id: 16, src: mysql, alt: 'MySQL' },
+  { id: 17, src: mongodb, alt: 'MongoDB' },
+  { id: 18, src: linux, alt: 'Linux' },
+];
+
+export default skillIcons;
