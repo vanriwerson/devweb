9be0c7d17c0b8a463840383dda031d6e460ee316
Bruno Riwerson Silva <riwerson@gmail.com>
2023-06-29 10:53:13 -0300

    feat: updating thumbs

--- src/utils/favoriteProjects.js
+++ src/utils/favoriteProjects.js
@@ -1,20 +1,51 @@
 import stomatopoda from '../assets/images/projects/stomatopoda.png';
 import stopwatch from '../assets/images/projects/stopwatch.png';
 import wallet from '../assets/images/projects/wallet.png';
+import crypto from '../assets/images/projects/crypto.png';
+import roman from '../assets/images/projects/roman.png';
 import calculators from '../assets/images/projects/calculators.png';
+import portfolio from '../assets/images/projects/portfolio.png';
 import movies from '../assets/images/projects/movies.png';
 import recipes from '../assets/images/projects/recipes.png';
 
 const favoriteProjects = [
-  { id: 517796165 , title: 'Lagosta Boxeadora', thumb: stomatopoda },
-  { id: 495821065 , title: 'Cronômetro Regressivo', thumb: stopwatch },
-  { id: 518853463 , title: 'Carteira Digital', thumb: wallet },
-  { id: 558788398 , title: 'Crypto Upvoting Api', thumb: null },
-  { id: 604103281 , title: 'Roman Numerals Converter', thumb: null },
-  { id: 506205000 , title: 'Calculadoras', thumb: calculators },
-  { id: 631986860 , title: 'Meu Portifólio', thumb: null },
-  { id: 446437823 , title: 'Site de Filmes', thumb: movies },
-  { id: 523802605 , title: 'Recipes App', thumb: recipes },
+  {
+    id: 517796165, 
+    title: 'Lagosta Boxeadora',
+    thumb: stomatopoda,
+  },
+  { id: 495821065, 
+    title: 'Cronômetro Regressivo',
+    thumb: stopwatch,
+  },
+  { id: 518853463, 
+    title: 'VanR Wallet',
+    thumb: wallet,
+  },
+  { id: 558788398, 
+    title: 'Crypto Upvoting Api',
+    thumb: crypto,
+  },
+  { id: 604103281, 
+    title: 'Roman Numerals Converter',
+    thumb: roman,
+  },
+  { id: 506205000, 
+    title: 'Calculadoras',
+    thumb: calculators,
+  },
+  { id: 631986860, 
+    title: 'Meu Portifólio',
+    thumb: portfolio,
+  },
+  { id: 446437823, 
+    title: 'Site de Filmes',
+    thumb: movies,
+  },
+  { id: 523802605, 
+    title: 'Recipes App',
+    thumb: recipes,
+  },
 ];
 
 export default favoriteProjects;
