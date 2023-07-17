43b2b19d62371b2ecf5c6ed85b11f1492b52b129
Bruno Riwerson Silva <riwerson@gmail.com>
2023-07-17 20:06:24 -0300

    feat: finishing projects section

---
+++ src/assets/textContent/aboutMyProjects.js
@@ -0,0 +1,6 @@
+const aboutMyProjects = [
+  'Ao longo da minha jornada acadêmico-profissional, tenho me dedicado a aprimorar minhas habilidades em diversas tecnologias e frameworks, buscando sempre aprender e evoluir, criando soluções eficientes e elegantes para problemas complexos.',
+  'Os projetos que escolhi destacar refletem minha caminhada e evolução como pessoa desenvolvedora, cada um deles sendo uma oportunidade de aprendizado e superação, onde pude aplicar minhas habilidades técnicas e criativas para entregar resultados de qualidade.',
+];
+
+export default aboutMyProjects;
